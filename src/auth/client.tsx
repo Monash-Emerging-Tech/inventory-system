b448881f9271115e1cc831e57f232c6a5e12217f
Hamish Welte <113832075+hamwel@users.noreply.github.com>
2026-05-01 03:26:36 +0000

    Remove authentik from auth client

--- src/auth/client.tsx
+++ src/auth/client.tsx
@@ -1,17 +1,9 @@
 import { getBaseUrl } from "@/lib/utils";
 import { createAuthClient } from "better-auth/react";
-import {
-  adminClient,
-  customSessionClient,
-  genericOAuthClient,
-} from "better-auth/client/plugins";
+import { adminClient, customSessionClient } from "better-auth/client/plugins";
 import type { auth } from "@/server/auth";
 
 export const authClient = createAuthClient({
-  plugins: [
-    adminClient(),
-    customSessionClient<typeof auth>(),
-    genericOAuthClient(),
-  ],
+  plugins: [adminClient(), customSessionClient<typeof auth>()],
   baseURL: getBaseUrl(),
 });
