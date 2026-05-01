bc41360002f7c867d1a44182bad7f1f45fc7bc33
Hamish Welte <113832075+hamwel@users.noreply.github.com>
2026-05-01 02:58:50 +0000

    Usage of Google Auth

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,59 +1,15 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "@/server/lib/prisma";
-import { admin, customSession, genericOAuth } from "better-auth/plugins";
+
 export const auth = betterAuth({
-  plugins: [
-    customSession(async ({ user, session }) => {
-      const role = await prisma.user
-        .findUnique({ where: { id: user.id } })
-        .then((x) => x?.role);
-      return {
-        user: {
-          ...user,
-          role: role,
-        },
-        session,
-      };
-    }),
-    admin(),
-    genericOAuth({
-      config: [
-        {
-          providerId: "authentik", // Matches the callback URL slug
-          clientId: process.env.AUTHENTIK_CLIENT_ID ?? "",
-          clientSecret: process.env.AUTHENTIK_CLIENT_SECRET ?? "",
-          discoveryUrl:
-            "https://auth.monashautomation.com/application/o/inventory-system/.well-known/openid-configuration",
-          scopes: ["openid", "profile", "email"],
-        },
-      ],
-    }),
-  ],
-  emailAndPassword: {
-    enabled: false,
-  },
-  // TO HAVE AN EMAIL WHITELIST
-  // databaseHooks: {
-  //   user: {
-  //     create: {
-  //       before: async (user) => {
-  //         const email = user.email?.toLowerCase() ?? "";
-  //
-  //         if (!(process.env.ALLOWED_EMAILS ?? "").split(",").includes(email)) {
-  //           throw new APIError("BAD_REQUEST", {
-  //             message: "User not allowed to use platform.",
-  //           });
-  //         }
-  //         return {
-  //           data: user,
-  //         };
-  //       },
-  //     },
-  //   },
-  // },
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
-  trustedOrigins: [process.env.FRONTEND_URL ?? "http://localhost:5173"],
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+  },
 });
